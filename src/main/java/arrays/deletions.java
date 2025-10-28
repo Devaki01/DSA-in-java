7d3495827b0c79980fb7e98c06a1507e51f4ce1c
Devaki01 <devakijoshi.2005@gmail.com>
2025-10-28 21:21:51 +0530

    array_deletion

--- src/main/java/arrays/deletions.java
+++ src/main/java/arrays/deletions.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 
 public class deletions {
     public static void main(String[] args) {
-
         int[] arr = new int[]{1, 1, 2, 3, 4, 5};
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println("deleting 1....");
+        System.out.println("Original array: " + Arrays.toString(arr));
+        System.out.println("Deleting element at index 1....");
 
         int pos = 1;
 
@@ -16,7 +15,7 @@ public class deletions {
             arr[i] = arr[i+1];
         }
 
-        System.out.println(Arrays.toString(arr));
-
+        int[] newArr = Arrays.copyOf(arr, arr.length - 1);
+        System.out.println("Final new array: " + Arrays.toString(newArr));
     }
 }
