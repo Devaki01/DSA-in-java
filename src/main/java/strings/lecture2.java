fbdce581b741c534314390b5181520c0453cd3d4
Devaki01 <devakijoshi.2005@gmail.com>
2025-12-04 19:26:59 +0530

    strings:comparison

--- src/main/java/strings/lecture2.java
+++ src/main/java/strings/lecture2.java
@@ -1,4 +1,17 @@
 package strings;
 
 public class lecture2 {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "hello"; //same object as a
+
+        System.out.println(a == b); //comparator, checks value as well as ref variable/where the obj is stored
+        System.out.println(a.equals(b)); //checks only the value
+
+        String c = new String("hello"); //creates new string hello, new  object
+
+        System.out.println(a == c); //comparator
+        System.out.println(a.equals(c));
+
+    }
 }
--- src/main/java/strings/lecture3.java
+++ src/main/java/strings/lecture3.java
@@ -1,4 +1,9 @@
 package strings;
 
 public class lecture3 {
+    public static void main(String[] args) {
+        String a = "hello world";
+
+        System.out.println(a.charAt(0)); //to print char at a particular index
+    }
 }
